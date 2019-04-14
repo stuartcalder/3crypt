dbdefb7357031a70fecbd9ffeced9e4b5dd208d6
Stuart <notstuart@gmail.com>
2019-04-14 12:25:37 -0700

    fixed one bug

--- vgp.cpp
+++ vgp.cpp
@@ -84,22 +84,22 @@ void VGP::cbc_decrypt_file(const char * const input_filename, const char * const
                        "Output file is: %p\n", input_file, output_file );
     }
   }
-  const size_t input_file_size = get_file_size( input_file ); // get the size of the input file
+  size_t bytes_to_read = get_file_size( input_file );
   //Check if the file is the right size to be decrypted
-  if( input_file_size % Block_Bytes != 0 ) {
-    fprintf( stderr, "The input file does not appear to be the right size to be decrypted.\n"
-                     "The file was %zu bytes.\n", input_file_size );
+  if( bytes_to_read % Block_Bytes != 0 ) {
+    fprintf( stderr, "The input file does ont appear to be the right size to be decrypted.\n"
+                     "The file was %zu bytes.\n", bytes_to_read );
     exit( 1 );
   }
   //Get the initialization vector
   {//+
     uint8_t file_iv[ Block_Bytes ];
-    fread( file_iv, 1, sizeof(file_iv), input_file );
+    bytes_to_read -= fread( file_iv, 1, sizeof(file_iv), input_file );
     cbc.manually_set_state( file_iv );
   }//-
   //Decrypt up to the last block
   uint8_t buffer[ Block_Bytes * 2 ];
-  const size_t last_input_block_offset = input_file_size - Block_Bytes;
+  const size_t last_input_block_offset = (bytes_to_read > Block_Bytes) ? (bytes_to_read - Block_Bytes) : 0;
   for( size_t b_off = 0; b_off < last_input_block_offset; b_off += Block_Bytes ) {
     fread( buffer, 1, Block_Bytes, input_file );
     cbc.decrypt_no_padding( buffer, buffer, Block_Bytes );
