375a9824c85948150fe36fb0923f24390969e6bb
Stuart <notstuart@gmail.com>
2019-04-16 22:02:04 -0700

    static function

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main()
     uint8_t key[64];
     uint8_t iv [64];
     //Generate a random key
-    vgp.generate_random_bytes( key, sizeof(key) );
-    vgp.generate_random_bytes( iv, sizeof(iv) );
+    VGP::generate_random_bytes( key, sizeof(key) );
+    VGP::generate_random_bytes( iv, sizeof(iv) );
     //Generate a random IV
     vgp.cbc_encrypt_file( "original_file", "encrypted_file", key, iv, (1024 * 1024 * 20) );
     vgp.cbc_decrypt_file( "encrypted_file", "decrypted_file", key, (1024 * 1024 * 20) );
