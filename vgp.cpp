4dcae10d8cbfeac2b2633f48a3f652d4bdb929e4
Stuart <notstuart@gmail.com>
2019-04-20 21:28:25 -0700

    move file-encryption code out of the class. Built a system for parsing arguments into VGP.

--- vgp.cpp
+++ vgp.cpp
@@ -2,118 +2,6 @@
 #include "vgp.hpp"
 #include <unistd.h>
 
-void VGP::cbc_encrypt_file(const char * const input_filename, const char * const output_filename,
-                           const uint8_t * const key, const uint8_t * const iv,
-                           const size_t file_buffer_size)
-{
-  using namespace std;
-  //Validate arguments somewhat
-  if( (key == nullptr) || (iv == nullptr) ) { // Diallow key or iv from being nullptr. That wouldn't make sense.
-    fprintf( stderr, "ERROR: VGP::encrypt_file -- Either the key or the initialization vector was a nullptr\n"
-                     "The Key: %p\n"
-                     "The IV : %p\n", key, iv );
-    exit( 1 );
-  }
-
-  //Open the input file, and the file to write to.
-  cbc_t cbc{ Threefish_t{ reinterpret_cast<const uint64_t*>(key) } }; // feed key
-  cbc.manually_set_state( iv );                // & iv into the cbc_t object
-  FILE * const input_file = fopen ( input_filename , "rb" );   // open the input file
-  FILE * const output_file = fopen( output_filename, "wb" ); // open the output file
-  //Check if files successfully opened
-  if( (input_file == nullptr) || (output_file == nullptr) ) {
-    fprintf( stderr, "Failed to open input file or output file\n"
-                     "Input file is: %p\n"
-                     "Output file is: %p\n", input_file, output_file );
-    exit( 1 );
-  }
-  size_t bytes_to_encrypt = get_file_size( input_file );           // see how big the file is
-  auto buffer = make_unique<uint8_t[]>( file_buffer_size );
-  //Write the IV into the beginning of the output file.
-  fwrite( iv, 1, Block_Bytes, output_file );
-  cbc.manually_set_state( iv );
-  while( bytes_to_encrypt > file_buffer_size ) {
-    fread( buffer.get(), file_buffer_size, 1, input_file );
-    cbc.encrypt_no_padding( buffer.get(), buffer.get(), file_buffer_size );
-    fwrite( buffer.get(), file_buffer_size, 1, output_file );
-    bytes_to_encrypt -= file_buffer_size;
-  }
-  {//+
-    fread( buffer.get(), 1, bytes_to_encrypt, input_file );
-    size_t encrypted = cbc.encrypt( buffer.get(), buffer.get(), bytes_to_encrypt );
-    fwrite( buffer.get(), 1, encrypted, output_file );
-  }//-
-  //Cleanup
-  explicit_bzero( buffer.get(), file_buffer_size );
-  fclose( input_file );
-  fclose( output_file );
-}
-
-void VGP::cbc_decrypt_file(const char * const input_filename, const char * const output_filename, const uint8_t * const key,
-                           const size_t file_buffer_size)
-{
-  using namespace std;
-
-
-  //Validate arguments somewhat
-  {//+
-    if( (key == nullptr) ) { // Diallow key from being nullptr. That wouldn't make sense.
-      fprintf( stderr, "ERROR: VGP::encrypt_file -- Either the key or the initialization vector was a nullptr\n"
-                       "The Key: %p\n", key );
-      exit( EXIT_FAILURE );
-    }
-  }//-
-
-  /////////////////Open the input file and the output file////////////////////////////
-  cbc_t cbc{ Threefish_t{ reinterpret_cast<const uint64_t*>(key) } };
-  FILE * const input_file = fopen( input_filename, "rb" ); // open the input file
-  FILE * const output_file = fopen( output_filename, "wb" ); // open the output file
-  //////////////////Check if files successfully opened///////////////////////////////
-  if( (input_file == nullptr) || (output_file == nullptr) ) {
-    fprintf( stderr, "Failed to open input file or output file\n"
-                     "Input file is: %p\n"
-                     "Output file is: %p\n", input_file, output_file );
-    exit( EXIT_FAILURE );
-  }
-  ////////////////////Check if parameters make sense//////////////////////
-  size_t bytes_to_decrypt = get_file_size( input_file );
-  if( bytes_to_decrypt < (Block_Bytes * 2) ) {
-    fprintf( stderr, "Error: The input file does not appear to be big enough to have been Threefish-512-CBC encrypted.\n" );
-    exit( EXIT_FAILURE );
-  }
-  if( (bytes_to_decrypt % Block_Bytes) != 0 ) {
-    fprintf( stderr, "Error: The input files does not appear to be a multiple of Threefish-512-CBC encrypted blocks.\n" );
-    exit( EXIT_FAILURE );
-  }
-  if( (file_buffer_size % Block_Bytes) != 0 ) {
-    fprintf( stderr, "Error: The file buffer size must be a multiple of 64-bytes.\n" );
-    exit( EXIT_FAILURE );
-  }
-  //////////////////////////////Get the initialization vector///////////////////////
-  {//+
-    uint8_t file_iv[ Block_Bytes ];
-    bytes_to_decrypt -= fread( file_iv, 1, sizeof(file_iv), input_file );
-    cbc.manually_set_state( file_iv );
-  }//-
-  ///////////////////////////////Decrypt/////////////////////////////////////
-  auto buffer = make_unique<uint8_t[]>( file_buffer_size );
-  while( bytes_to_decrypt > file_buffer_size ) {
-    fread( buffer.get(), file_buffer_size, 1, input_file );
-    cbc.decrypt_no_padding( buffer.get(), buffer.get(), file_buffer_size );
-    fwrite( buffer.get(), file_buffer_size, 1, output_file );
-    bytes_to_decrypt -= file_buffer_size;
-  }
-  {//+
-    fread( buffer.get(), 1, bytes_to_decrypt, input_file );
-    size_t last = cbc.decrypt( buffer.get(), buffer.get(), bytes_to_decrypt );
-    fwrite( buffer.get(), 1, last, output_file );
-  }//-
-  ///////////////////////////////////////Cleanup/////////////////////////////////////
-  explicit_bzero( buffer.get(), file_buffer_size );
-  fclose( input_file );
-  fclose( output_file );
-}
-
 VGP::VGP(const int argc, const char * argv[])
 {
   //Get a mapping of the c args
@@ -191,4 +79,21 @@ void VGP::print_help()
     "-i, --input-file\t\tInput file; Must be specified for symmetric encryption and decryption modes.\n"
     "-o, --output-file\t\tOutput file; For symmetric encryption and decryption modes. Optional."
   );
+}
+
+void VGP::symmetric_encrypt_file() const
+{
+  std::string input_filename, output_filename;
+  //////////Get the input and output filenames///////////////////////
+  for( const auto & pair : _option_argument_pairs ) {
+    check_file_name_sanity( pair.second );
+    if( pair.first == "-i" || pair.first == "--input-file" ) {
+      input_filename = pair.second;
+    }
+    else if( pair.first == "-o" || pair.second == "--output-file" ) {
+      output_filename = pair.second;
+    }
+  }
+  //////////Get the input and output filenames///////////////////////
+  //TODO 
 }
--- vgp.hpp
+++ vgp.hpp
@@ -1,8 +1,10 @@
 #pragma once
+#include "include/general/arg_mapping.hpp"
+#include "include/crypto/operations.hpp"
 #include "include/crypto/threefish_precomputed_keyschedule.hpp"
 #include "include/crypto/cbc.hpp"
+#include "include/crypt/file_encryption.hpp"
 #include "include/files/files.hpp"
-#include "include/general/arg_mapping.hpp"
 #include <cstdlib>
 #include <memory>
 #include <string>
@@ -13,13 +15,10 @@ class VGP
 public:
 /* PUBLIC CONSTANTS */
   using Threefish_t = Threefish_Precomputed_Keyschedule<512>;
-  using cbc_t = CBC< Threefish_t, Threefish_t::Key_Bits >;
   enum class Mode {
     None, Encrypt_File, Decrypt_File
   };
   static constexpr const size_t Block_Bytes = (Threefish_t::Number_Words * 8);
-  static constexpr const size_t Default_File_Buffer_Size = 1024 * 1024 * 10; // 10 MiB
-  static constexpr const bool   Debug = true;
 /* CONSTRUCTOR(S) */
   VGP(const int argc, const char * argv[]);
 private:
@@ -27,15 +26,9 @@ private:
   Mode                   _mode = Mode::None;
   Arg_Mapping::Arg_Map_t _option_argument_pairs;
 /* PRIVATE FUNCTIONS */
-  void generate_random_bytes(uint8_t * const buffer, size_t num_bytes) const;
-  void cbc_encrypt_file(const char * const input_filename, const char * const output_filename,
-                        const uint8_t * const key, const uint8_t * const iv,
-                        const size_t file_buffer_size = Default_File_Buffer_Size);
-  void cbc_decrypt_file(const char * const input_filename, const char * const output_filename,
-                        const uint8_t * const key,
-                        const size_t file_buffer_size = Default_File_Buffer_Size);
   void process_arg_mapping(const Arg_Mapping::Arg_Map_t & a_map);
   inline auto get_mode_c_str(const Mode m) const -> const char *;
   void set_mode(const Mode m);
   void print_help();
+  void symmetric_encrypt_file() const;
 };
