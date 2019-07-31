6148c64e77725cb4a4915b581940e64ac55d0807
Stuart <notstuart@gmail.com>
2019-07-31 09:29:43 -0700

    check to see if it's a CBC_V2 file first, before checking to see if it's a CBC_V1 file

--- determine_decrypt_method.cc
+++ determine_decrypt_method.cc
@@ -37,18 +37,6 @@ namespace threecrypt
                               bytes_to_read, read_bytes );
                 std::exit( EXIT_FAILURE );
             }
-// If the CBC_V1 header was included, check to see if the file is a CBC_V1 encrypted file.
-#ifdef CBC_V1_HH
-            {
-                using namespace cbc_v1;
-                static_assert(sizeof(CBC_V1_Header_t::id) == ssc::static_strlen(CBC_V1_ID));
-                if ( method == Decryption_Method_e::None &&
-                     memcmp( buffer, CBC_V1_ID, sizeof(CBC_V1_Header_t::id) ) == 0 )
-                {
-                    method = Decryption_Method_e::CBC_V1;
-                }
-            }
-#endif
 // If the CBC_V2 header was included, check to see if the file is a CBC_V2 encrypted file.
 #ifdef CBC_V2_HH
             {
@@ -58,6 +46,18 @@ namespace threecrypt
                      memcmp( buffer, CBC_V2_ID, sizeof(CBC_V2_Header_t::id) ) == 0 )
                 {
                     method = Decryption_Method_e::CBC_V2;
+                }
+            }
+#endif
+// If the CBC_V1 header was included, check to see if the file is a CBC_V1 encrypted file.
+#ifdef CBC_V1_HH
+            {
+                using namespace cbc_v1;
+                static_assert(sizeof(CBC_V1_Header_t::id) == ssc::static_strlen(CBC_V1_ID));
+                if ( method == Decryption_Method_e::None &&
+                     memcmp( buffer, CBC_V1_ID, sizeof(CBC_V1_Header_t::id) ) == 0 )
+                {
+                    method = Decryption_Method_e::CBC_V1;
                 }
             }
 #endif
