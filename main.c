ec64d3243e564f9866ba44abf2bb0874d7f5a9dc
Stuart Calder <technoglub@protonmail.com>
2020-12-12 22:55:12 -0800

    Unveil /usr

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int
 main (int argc, char ** argv)
 {
+	SHIM_OPENBSD_UNVEIL ("/usr", "r");
 	SHIM_OPENBSD_PLEDGE ("stdio unveil rpath wpath cpath tty", NULL);
 	threecrypt( argc, argv );
 	return EXIT_SUCCESS;
